87918ea2dc1c5f1b306c16087ca02fc99c893092
nastya <chest.nastya@gmail.com>
2019-11-08 18:05:09 +0300

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  htmlWhitespaceSensitivity: "ignore",
+};
