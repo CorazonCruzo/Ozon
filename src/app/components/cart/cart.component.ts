075034f5689529f21243b0907cc48bc2e9273e9b
nastya <chest.nastya@gmail.com>
2019-11-09 18:35:40 +0300

    realize logic for checkbox

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, AfterViewInit {
+  @ViewChild('typeAll', {static: false}) typeAll: ElementRef;
+  @ViewChildren('input') inputs: QueryList<ElementRef>;
+
+  isCheckedAll = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    if (this.isCheckedAll) {
+      this.inputs.forEach(item => {
+        item.nativeElement.checked = true;
+      });
+    }
+  }
+
+  get countOfProducts() {
+    return this.inputs.length;
+  }
+
+  onChange() {
+    const inputsChecked = this.inputs.filter( item => item.nativeElement.checked === true);
+
+    if (inputsChecked.length !== this.countOfProducts) {
+      this.isCheckedAll = false;
+    } else {
+      this.isCheckedAll = true;
+    }
+  }
+
+  onCheckAll() {
+    if (this.typeAll.nativeElement.checked) {
+      this.inputs.forEach( item => {
+        item.nativeElement.checked = true;
+      });
+    } else {
+      this.inputs.forEach( item => {
+        item.nativeElement.checked = false;
+      });
+    }
+  }
 }
