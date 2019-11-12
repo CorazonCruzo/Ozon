ad1908fbe73801b019c78271c4c4137bb68517ba
nastya <chest.nastya@gmail.com>
2019-11-12 12:17:46 +0300

    modificate model: add new properties

--- src/app/shared/models/product.ts
+++ src/app/shared/models/product.ts
@@ -1,9 +1,21 @@
 export class Product {
-  constructor(
-    public id: string,
-    public title: string,
-    public price: number,
-    public isChecked: boolean = false,
-    public isInCart: boolean = false
-  ) {}
+  public id: string;
+  public title: string;
+  public image: string;
+  public price: number;
+  public isChecked: boolean;
+  public isInCart: boolean;
+  public discount: number;
+  public rating: number;
+
+  constructor({id, title, image, price, isChecked = true, isInCart = false, discount = 0, rating = 4}) {
+    this.id = id;
+    this.title = title;
+    this.image = image;
+    this.price = price;
+    this.isChecked = isChecked;
+    this.isInCart = isInCart;
+    this.discount = discount;
+    this.rating = rating;
+  }
 }
