74bfc105973e017b82b6dfe6a567a61a8d400468
nastya <chest.nastya@gmail.com>
2019-11-12 12:26:10 +0300

    root files update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {CatalogComponent} from "./components/catalog/catalog.component";
-import {CartComponent} from "./components/cart/cart.component";
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
   { path: 'catalog', component: CatalogComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { CartComponent } from './components/cart/cart.component';
-import { PricePipe } from './shared/pipes/price-pipe.pipe';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import {FormsModule} from '@angular/forms';
-import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { CheckAllBoxComponent } from './components/check-all-box/check-all-box.component';
 import { DeleteSelectedBoxComponent } from './components/delete-selected-box/delete-selected-box.component';
 import { PriceComponent } from './components/price/price.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { StorageServiceModule } from 'angular-webstorage-service';
+import { StarRatingModule } from 'angular-star-rating';
 
 @NgModule({
-  declarations: [AppComponent, CatalogComponent, CartComponent, PricePipe, NavBarComponent, ProductListComponent, ProductItemComponent, CheckAllBoxComponent, DeleteSelectedBoxComponent, PriceComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, CatalogComponent, CartComponent, NavBarComponent, ProductItemComponent, CheckAllBoxComponent, DeleteSelectedBoxComponent, PriceComponent, ProductListComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, StorageServiceModule, StarRatingModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
